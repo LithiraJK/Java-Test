11d6c186adfb2c4739bf484570d4ddbe754eec04
LithiraJK <kariyawasamlithira2@gmail.com>
2024-08-17 19:44:35 +0530

    first commit

--- StringFormatting.java
+++ StringFormatting.java
@@ -3,19 +3,19 @@ public class StringFormatting {
     String str = "Ada nam mara kammali lamai. api next week ekema oop karamu.. ";
      String s = String.format("%300s", str);
    
-     System.out.println(s);
+     //System.out.println(s);
  
  
     String name = "Jason Staitham";
-    int age = 56;
+    int age = -56;
     float salary = 150000.58799f;
     int cid = 1;
  
  
-    //System.out.println("Name ="+name + "Age = "+ age+ "Salary = "+salary+ "ID = "+cid);
+    System.out.println("Name ="+name + "Age = "+ age+ "Salary = "+salary+ "ID = "+cid);
  
  
-   // System.out.printf("ID : C%03d, Name : %s , Age : %d, Salary : %,.3f",cid,name,age,salary);
+   System.out.printf("ID : C%03d, Name : %s , Age : %d, Salary : %,.3f",cid,name,age,salary);
  }
  }
  
